99c7e8fd5f7024b7d7d28d5fbcff6c3a3df7fc78
Kirby Kuznia <kkuznia@gmail.com>
2019-01-14 17:50:18 -0700

    Started working on the real shuffle, I am completely dropping the perfect shuffle

--- src/PerfectShuffle.java
+++ src/PerfectShuffle.java
@@ -1,8 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PerfectShuffle {
+	private static final int RAND_BOUND = 4;
 	private List<Card> cards = new ArrayList<Card>(52);
+	private Random generator = new Random();
 	
 	public PerfectShuffle(List<Card> start){
 		cards = start;
@@ -17,15 +18,26 @@ public class PerfectShuffle {
 	
 	//Perform one round of shuffling
 	//Which cuts the deck into 2 parts then shuffles
-	//By taking one from a deck then another from the other
+	//By taking a random number of cards from one deck
+	//Then a random number from the other
 	private List<Card> shuffleOnce(){
-		List<Card> deck1 = cards.subList(0, 26);
-		List<Card> deck2 = cards.subList(26, 52);
+		List<Card> deck1 = cards.subList(0, 26); Stack<Card> d1 = (Stack<Card>) deck1;
+		List<Card> deck2 = cards.subList(26, 52);Stack<Card> d2 = (Stack<Card>) deck2;
 		List<Card> newCards = new ArrayList<Card>(52);
+		int remainingD1 = 26, remainingD2 = 26;
 		//This is the shuffling loop
-		for(int i = 0; i < 26; i++){
-			newCards.add(deck1.get(i));
-			newCards.add(deck2.get(i));
+		while(remainingD1 >= 0 && remainingD2 >= 0){
+			int n1 = generator.nextInt(RAND_BOUND), n2 = generator.nextInt(RAND_BOUND);
+			//After we determine the random number of cards we're using we have to do some checks
+			//This means we wanted more than there was less therefore the stack is out
+			if(n1 > remainingD1)
+				n1 = remainingD1;
+			if(n2 > remainingD2)
+				n2 = remainingD2;
+			for(int i = 1; i <= n1; i++)
+				newCards.add(d1.pop());
+			for(int i = 1; i <= n2; i++)
+				newCards.add(d2.pop());
 		}
 		cards = newCards;
 		return newCards;
